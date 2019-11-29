db72c9cf561556c7d37bd6adf0c682cb7a91f5d7
Juan Pablo Fernandez Tubello <juanfernatubello@gmail.com>
2019-11-29 14:28:31 -0300

    Update tw_bot.py

--- tw_bot.py
+++ tw_bot.py
@@ -15,17 +15,18 @@ auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
            
 #Minutos * Segundos * (Cuantas veces se repite = horas de intervalo)
-INTERVALO = 60 * 60 * 6 #Twittea cada 6 horas.
+#INTERVALO = 60 * 60 * 6 #Twittea cada 6 horas.
+INTERVALO = 1200
 
-while True:
-           
+while True:       
  with codecs.open('frases.txt', encoding='utf-8', errors='ignore') as myfile:
     frases = myfile.readlines()
            
  #Se genera un numero random el cual es utilizado como indice
  r = random.randint(0, 24)
- tweet = "@CamiMontielH " + frases[r]
-
+ #tweet = "@CamiMontielH " + frases[r]
+  tweet = frases[r]
+           
  #Twitteamos
  try:
      if api.update_status(tweet):
