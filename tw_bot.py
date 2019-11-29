8c73229a2f53a551510f7a64a4bd34dd8b509fad
Juan Pablo Fernandez Tubello <juanfernatubello@gmail.com>
2019-11-29 01:34:43 -0300

    Add files via upload

---
+++ tw_bot.py
@@ -0,0 +1,37 @@
+# coding=utf-8
+
+import tweepy
+import random
+import os
+
+CONSUMER_KEY = os.getenv('CONSUMER_KEY')
+CONSUMER_SECRET = os.getenv('CONSUMER_SECRET')
+ACCESS_KEY = os.getenv('ACCESS_KEY')
+ACCESS_SECRET = os.getenv('ACCESS_SECRET')
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY,CONSUMER_SECRET) 
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET) 
+api = tweepy.API(auth)
+           
+      #Minutos * Segundos * (Cuantas veces se repite = horas de intervalo)
+ INTERVALO = 60 * 60 * 6 #Twittea cada 6 horas.
+
+while True:
+
+ #Obtenemos las frases del archivo de frases
+ with open ("frases.txt", "r") as myfile:
+    frases = myfile.readlines()
+
+ #Se genera un numero random el cual es utilizado como indice
+ r = random.randint(0, 24)
+ tweet = "@CamiMontielH " + frases[r]
+
+
+ try:
+     if api.update_status(tweet):
+         print("Twitteado")
+ #Manejo de excepciones 
+ except tweepy.error.TweepError as e:
+     print(e)
+
+ time.sleep(INTERVALO)
