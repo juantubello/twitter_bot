cc8659b4a04047abf0ade60c9e41b2996016c482
Juan Pablo Fernandez Tubello <juanfernatubello@gmail.com>
2019-12-19 23:32:57 -0300

    Update tw_bot.py

--- tw_bot.py
+++ tw_bot.py
@@ -1,4 +1,3 @@
-
 import tweepy
 import random
 import os
@@ -13,24 +12,69 @@ ACCESS_SECRET = os.getenv('ACCESS_SECRET')
 auth = tweepy.OAuthHandler(CONSUMER_KEY,CONSUMER_SECRET) 
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET) 
 api = tweepy.API(auth)
-           
+
+#Flag para identificar tweet's twitteados
+flag_twitteado = False
+
+indiceDeUltimaFrase = 100
+
 #Minutos * Segundos * (Cuantas veces se repite = horas de intervalo)
-INTERVALO = 60 * 60 * 6 #Twittea cada 6 horas.
+INTERVALO = 60 * 10 #Responde cada 10 minutos.
 
-while True:       
- with codecs.open('frases.txt', encoding='ISO-8859-1', errors='ignore') as myfile:
-    frases = myfile.readlines()
-           
- #Se genera un numero random el cual es utilizado como indice
- r = random.randint(0, 24)
- tweet = "@CamiMontielH " + frases[r]
+while True:
 
- #Twitteamos
- try:
-     if api.update_status(tweet):
-         print("Twitteado")
- #Manejo de excepciones 
- except tweepy.error.TweepError as e:
-     print(e)
+ #Obtengo los tweets correspondientes a "CoscuBot"
+ busqueda = api.search("CoscuBot")
+
+ for tweet in busqueda:
+
+     flag_twitteado = False
+ 
+     #Id del tweet
+     replyId = tweet.id
+ 
+     #Id del tweet en string
+     replyIdStr = tweet.id_str
+
+     #Usuario que menciono al bot
+     username = tweet.user.screen_name
+
+     #Ignoro mis propios tweets
+     if username == 'CoscuBot':
+         print("Tweet ID: ",replyIdStr, " propio")
+         continue
+
+     #Valido no haber respondido previamente a ese tweet mediante el log .txt
+     log_file = open('IDlog.txt','r+') 
+     id_logs = log_file.readlines()
+     for tw_id in id_logs:
+         tw = tw_id.strip().split(',')
+         if replyIdStr == tw[-2]:
+             print("Tweet ID: ", replyId, " ya respondido" )
+             flag_twitteado = True
+
+     if not flag_twitteado:
+         #Leeo las Quotes de Coscu
+         with codecs.open('coscuQuotes.txt', encoding='ISO-8859-1', errors='ignore') as myfile:
+             frases = myfile.readlines()
+         myfile.close()      
+
+     #Genero un numero random para usarlo como indice en la lista de Quotes
+     r = random.randint(0, 18)
+     while(r == indiceDeUltimaFrase):
+         r = random.randint(0, 18)
+     indiceDeUltimaFrase = r
+     tweet = "@" + username +" "+ frases[r]
+     
+     #Twitteamos
+     try:
+       if api.update_status(status = tweet, in_reply_to_status_id = replyId) :
+         log_file.writelines( (replyIdStr + ","))
+         log_file.close()
+         print("Twitteado con éxito")
+     #Manejo de excepciones 
+     except tweepy.error.TweepError as e:
+         print(e)
 
  time.sleep(INTERVALO)
+
